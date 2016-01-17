23a1953c6321ea15034707d46d06783239b106a1
Lincoln Yu <quanben@msn.com>
2016-01-17 22:02:26 +1000

    enhanced the tracking ui with better coloring and UI selection.

---
+++ ParentingTrackerApp/ParentingTrackerApp/Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ParentingTrackerApp.Converters
+{
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var b = (bool)value;
+            return b ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
